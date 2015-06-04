3b0c8394b5908857c410499a079baedeb72d51ae
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-04 17:25:49 +0100

    basic install of bowtie-1.1.1 for use with deFuse

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name 'Bowtie'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'Eagle Genomics'
+maintainer_email 'info@eaglegenomics.com'
+license 'apache2'
 description 'Installs/Configures Bowtie'
 long_description 'Installs/Configures Bowtie'
 version '0.1.0'
+
+supports 'centos', '= 6.5'
+
+depends 'build-essential'
