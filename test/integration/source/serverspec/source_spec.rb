e9c42dfc63f88c79d3eb26e88f6f2716cb8b1823
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-26 15:32:53 +0100

    Fixed some rubocop offences

--- test/integration/source/serverspec/source_spec.rb
+++ test/integration/source/serverspec/source_spec.rb
@@ -12,13 +12,13 @@ puts 'Bowtie version = ' + pseudo_node.default['Bowtie']['version']
 # tests for executables
 %w(bowtie-inspect-s bowtie-align-l bowtie-align-s bowtie-build-l bowtie-build-s bowtie bowtie-build
    bowtie-inspect).each do |file_executable|
-	describe command("which #{pseudo_node.default['Bowtie']['install_path']}/" +
-		"#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable}") do
-  		its(:exit_status) { should eq 0 }
-	end
-	
-	describe command("#{pseudo_node.default['Bowtie']['install_path']}/" +
-		"#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable} --version") do
-	  	its(:stdout) { should match(/#{file_executable} version 1.1.1/) }
-	end
+  describe command("which #{pseudo_node.default['Bowtie']['install_path']}/" \
+  "#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable}") do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command("#{pseudo_node.default['Bowtie']['install_path']}/" \
+    "#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable} --version") do
+    its(:stdout) { should match(/#{file_executable} version 1.1.1/) }
+  end
 end
--- test/integration/source/serverspec/spec_helper.rb
+++ test/integration/source/serverspec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'serverspec'
 
+# Class to mimic node
 class PseudoNode
   attr_accessor :default
 
+  # MyHash
   class MyHash < Hash
     alias_method :orig_subscript, :'[]'
     define_method(:'[]') do |subscript|
@@ -10,12 +12,12 @@ class PseudoNode
       orig_subscript(subscript)
     end
   end
-  
-  def initialize config_path='/tmp/default_attributes.rb'
+
+  def initialize(config_path = '/tmp/default_attributes.rb')
     default = node = override = MyHash.new
 
     eval IO.read(config_path)
-   
+
     @default = default
   end
   # default hash has all the values now.
