444845e87cb9e65a2e5227ab5f295da492b9809d
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-10 16:48:44 +0100

    Fixed bug testing that allowed bug in recipe to succeed

--- test/integration/source/serverspec/source_spec.rb
+++ test/integration/source/serverspec/source_spec.rb
@@ -12,13 +12,11 @@ puts 'Bowtie version = ' + pseudo_node.default['Bowtie']['version']
 # tests for executables
 %w(bowtie-inspect-s bowtie-align-l bowtie-align-s bowtie-build-l bowtie-build-s bowtie bowtie-build
    bowtie-inspect).each do |file_executable|
-  describe command("which #{pseudo_node.default['Bowtie']['install_path']}/" \
-  "#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable}") do
+  describe command("which #{file_executable}") do
     its(:exit_status) { should eq 0 }
   end
 
-  describe command("#{pseudo_node.default['Bowtie']['install_path']}/" \
-    "#{pseudo_node.default['Bowtie']['dirname']}/#{file_executable} --version") do
+  describe command("#{file_executable} --version") do
     its(:stdout) { should match(/#{file_executable} version 1.1.1/) }
   end
 end
