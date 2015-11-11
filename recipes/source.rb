25f18082650d56be92dc95385d6751f1ccb2b604
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-11-11 14:03:27 +0000

    changed to docker

--- recipes/source.rb
+++ recipes/source.rb
@@ -3,12 +3,22 @@
 # Recipe:: source
 #
 # Copyright (c) 2015 Eagle Genomics, All Rights Reserved.
+##########################################################
+# package install
+
+package ['zlib-devel', 'tar', 'make', 'unzip', 'epel-release'] do
+  action :install
+end
+
+package ['clang'] do
+  action :install
+end
+
+##########################################################
 
 include_recipe 'build-essential'
 
-package 'unzip' do
-  action :install
-end
+##########################################################
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{node['Bowtie']['filename']}" do
   source node['Bowtie']['url']
@@ -29,7 +39,11 @@ end
 execute "find #{node['Bowtie']['install_path']}/#{node['Bowtie']['dirname']} -maxdepth 1 -name 'bowtie*' -executable -type f -exec ln -s {}  #{node['Bowtie']['install_path']}/bin \\;" do
 end
 
-# here for use by serverspec
+##########################################################
+# here for use by serverspecs
+
 magic_shell_environment 'BOWTIE_VERSION' do
   value node['Bowtie']['version']
 end
+
+##########################################################
