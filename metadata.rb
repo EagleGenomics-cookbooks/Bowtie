24bf84feeef8e79c6a0638b1678e7dad80e1b21d
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-06-23 14:44:48 +0100

    Supports centos78

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures Bowtie'
 long_description 'Installs/Configures Bowtie'
-version '1.0.1'
+version '1.0.2'
 
 supports 'centos', '= 6.5'
+supports 'centos', '= 7.0'
 supports 'ubuntu', '= 14.04'
 
 depends 'build-essential'
