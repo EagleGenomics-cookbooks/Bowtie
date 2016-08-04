0e7d7c250f2a12353ae5355776b0c7bc60b4abb5
Florian-EagleGenomics <florian.reisinger@eaglegenomics.com>
2016-08-04 11:23:45 +0100

    Extend unit tests and remove obsolete tests
    Add some non-default test cases to the unit tests to check the default
    recipe's actions. Remove the source unit tests, as they are obsolete
    and did not test anything real anyway.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,5 +16,17 @@ describe 'Bowtie::default' do
     it 'converges successfully' do
       chef_run # This should not raise an error
     end
+
+    it 'includes the `build-essential` recipe' do
+      expect(chef_run).to include_recipe('build-essential')
+    end
+
+    it 'installs clang, tar, unzip' do
+      expect(chef_run).to install_package 'clang, tar, unzip'
+    end
+
+    it 'adds a new magic_shell_environment' do
+      expect(chef_run).to add_magic_shell_environment('BOWTIE_VERSION')
+    end
   end
 end
