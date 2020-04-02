9268502d85f5be2d7d6d39a3f0330cff0115931c
nick-james <nick.james@eaglegenomics.com>
2020-04-02 18:05:24 +0100

    update locale usage

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,11 @@
 #
 # Copyright:: 2019, Eagle Genomics Ltd, All Rights Reserved.
 apt_update if node['platform_family'] == 'debian'
-include_recipe 'locale' if node['platform_family'] == 'debian'
+
+locale 'set locale' do
+  lang node['locale']['lang']
+  lc_all node['locale']['lc_all']
+end
 
 if node['platform_family'] == 'debian'
   package ['zlib1g-dev'] do
